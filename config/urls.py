91b852f4b7a122469d11ff7f0d563a589c27db1d
rafkix <rafkixuz@gmail.com>
2025-09-06 10:29:47 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 # config/urls.py
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),  # DRF login/logout
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
